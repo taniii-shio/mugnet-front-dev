2e8a22a8934f84e23f0d5691148fa90a4c8091a0
Naoto Shioya <shioya.naoto17@gmail.com>
2022-07-21 17:53:52 +0900

    add colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,7 @@
-// const tintColorLight = '#2f95dc';
-// const tintColorDark = '#fff';
+const colors = {
+  white: '#fff',
+  black: '#000',
+  gray300: '#CBD5E0',
+};
 
-// export default {
-//   light: {
-//     text: '#000',
-//     background: '#fff',
-//     tint: tintColorLight,
-//     tabIconDefault: '#ccc',
-//     tabIconSelected: tintColorLight,
-//   },
-//   dark: {
-//     text: '#fff',
-//     background: '#000',
-//     tint: tintColorDark,
-//     tabIconDefault: '#ccc',
-//     tabIconSelected: tintColorDark,
-//   },
-// };
+export default colors;
