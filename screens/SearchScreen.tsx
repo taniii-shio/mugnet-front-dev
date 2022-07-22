a40c46e056d0a86f57dbc0843cb3f13500bcc0c1
Naoto Shioya <shioya.naoto17@gmail.com>
2022-07-23 00:17:34 +0900

    fix search screen

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -5,6 +5,7 @@ import {
   FlatList,
   TouchableOpacity,
   ImageBackground,
+  TextInput,
 } from 'react-native';
 import Header from '../components/Header';
 
@@ -12,21 +13,24 @@ import recommendUsers from '../assets/data/recommendUsers';
 import colors from '../constants/colors';
 
 import { RootTabScreenProps } from '../types';
+import { Entypo } from '@expo/vector-icons';
 
 export default function SearchScreen({
   navigation,
 }: RootTabScreenProps<'Search'>) {
-  // const renderRecommendUserItem = ({ item }: any) => {
-  //   return (
-  //     <TouchableOpacity>
-  //       <ImageBackground
-  //         source={item.profilePicture}
-  //         style={styles.recommendUserItem}
-  //         imageStyle={styles.recommendUserItemImage}
-  //       ></ImageBackground>
-  //     </TouchableOpacity>
-  //   );
-  // };
+  const renderRecommendUserItem = ({ item }: any) => {
+    return (
+      <TouchableOpacity>
+        <ImageBackground
+          source={item.profilePicture}
+          style={styles.recommendUserItem}
+          imageStyle={styles.recommendUserItemImage}
+        />
+        <Text style={styles.recommendUserName}>{item.name}</Text>
+        <Text style={styles.recommendUserDesc}>{item.desc}</Text>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View style={styles.container}>
       {/* header */}
@@ -36,14 +40,20 @@ export default function SearchScreen({
       <View style={styles.recommendWrapper}>
         <Text style={styles.recommendTitle}>おすすめの友達</Text>
         <View style={styles.recommendUsersWrapper}>
-          {/* <FlatList
+          <FlatList
             data={recommendUsers}
             renderItem={renderRecommendUserItem}
             keyExtractor={(item) => item._id}
             horizontal
             showsHorizontalScrollIndicator={false}
-          /> */}
+          />
         </View>
+      </View>
+
+      {/* search */}
+      <View style={styles.searchWrapper}>
+        <Entypo name="magnifying-glass" size={28} color={colors.black} />
+        <TextInput />
       </View>
     </View>
   );
@@ -54,11 +64,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.white,
   },
+  // recommend
   recommendWrapper: {},
   recommendTitle: {
     fontFamily: 'inter_bold',
   },
   recommendUsersWrapper: {},
-  recommendUserItem: {},
-  recommendUserItemImage: {},
+  recommendUserItem: {
+    width: 300,
+    height: 400,
+  },
+  recommendUserItemImage: {
+    borderRadius: 8,
+  },
+  recommendUserName: {},
+  recommendUserDesc: {},
+  // search
+  searchWrapper: {},
 });
